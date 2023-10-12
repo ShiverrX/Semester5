27a634e4496ebd7c74bb7f4cc42f1c08b72591ed
Nando Shiver <127407730+ShiverrX@users.noreply.github.com>
2023-10-12 08:19:26 +0700

    Add files via upload

---
+++ Web2/penjumlahan.js
@@ -0,0 +1,8 @@
+function jumlahkan() {
+    var bilangan1 = parseFloat(document.getElementById("bilangan1").value);
+    var bilangan2 = parseFloat(document.getElementById("bilangan2").value);
+
+    var hasil = bilangan1 + bilangan2;
+
+    alert("Hasil Penjumlahan:" + hasil);
+}
